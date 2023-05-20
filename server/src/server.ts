160b31b3d84b7ad7d545983c1ab0c8d5f923b459
ClecioSousa00 <cleciosouza015@gmail.com>
2023-05-20 12:39:30 -0300

    files

---
+++ server/src/server.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config'
+
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+
+const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0', //0.0.0.0 para mobile e localhost para web
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on port http://localhost:3333')
+  })
