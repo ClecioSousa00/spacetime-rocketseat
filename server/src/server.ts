69df76379abb122277d034f66f7a589373b8bf88
ClecioSousa00 <cleciosouza015@gmail.com>
2023-05-20 15:08:45 -0300

    aula 4

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,20 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -17,6 +27,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
@@ -27,3 +38,34 @@ app
   .then(() => {
     console.log('🚀 HTTP server running on port http://localhost:3333')
   })
+
+
+// import 'dotenv/config'
+
+// import fastify from 'fastify'
+// import cors from '@fastify/cors'
+// import jwt from '@fastify/jwt'
+// import { memoriesRoutes } from './routes/memories'
+// import { authRoutes } from './routes/auth'
+
+// const app = fastify()
+
+// app.register(cors, {
+//   origin: true,
+// })
+
+// app.register(jwt, {
+//   secret: 'spacetime',
+// })
+
+// app.register(authRoutes)
+// app.register(memoriesRoutes)
+
+// app
+//   .listen({
+//     port: 3333,
+//     host: '0.0.0.0', //0.0.0.0 para mobile e localhost para web
+//   })
+//   .then(() => {
+//     console.log('🚀 HTTP server running on port http://localhost:3333')
+//   })
